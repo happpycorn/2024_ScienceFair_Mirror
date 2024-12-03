88c0a8c17ad3c0efebe295462e578d4ada233681
corn <happpy.corn.man@gmail.com>
2024-12-03 16:49:55 +0800

    data test

--- Script/_Blank_Experiments.py
+++ Script/_Blank_Experiments.py
@@ -4,6 +4,14 @@ import pandas as pd
 from tqdm import tqdm
 from Modules.Modules import MirrorFrame
 
+test_time = 10
+test_count = 100
+
+blank1 = r'Blank\blank_yellow.png' 
+blank2 = r'Blank\blank_blue.png' 
+
+save_path = r'exp_data.csv'
+
 mirrorFrame = MirrorFrame(
 
     line_width=5, 
@@ -13,39 +21,25 @@ mirrorFrame = MirrorFrame(
 
 coords = mirrorFrame.generateCoords()
 
-data = {
-
-    "Value" : [],
-    "Time"  : []
-}
+data = {} 
 
 max_value = 255*mirrorFrame.size_x*mirrorFrame.size_y*3
 
 num = 0
 
-for i in tqdm(range(10)):
+for i in tqdm(range(test_time)):
 
-    mirrorFrame.frame = frame = cv2.imread('Blank\\blank_red.png')
+    blank_name = blank1 if i % 2 == 0 else blank2
+    mirrorFrame.frame = frame = cv2.imread(blank_name)
+    data[i] = [0] * test_count
 
-    for num in range(num, num+100):
-
-        for coord in coords : mirrorFrame.drawLine(coord)
-
-        frame_minus = np.abs(frame.astype(np.int32)-mirrorFrame.canvas.astype(np.int32))
-
-        data["Value"].append(np.sum(frame_minus)/max_value)
-        data["Time"].append(num)
-    
-    mirrorFrame.frame = frame = cv2.imread('Blank\\blank_blue.png')
-
-    for num in range(num, num+100):
+    for j in range(test_count):
 
         for coord in coords : mirrorFrame.drawLine(coord)
 
         frame_minus = np.abs(frame.astype(np.int32)-mirrorFrame.canvas.astype(np.int32))
-        
-        data["Value"].append(np.sum(frame_minus)/max_value)
-        data["Time"].append(num)
+
+        data[i][j] = np.sum(frame_minus)/max_value
 
 df = pd.DataFrame(data)
-df.to_csv("experiment_data.csv")
+df.to_csv(save_path)
