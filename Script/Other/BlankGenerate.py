3e111658989582dd1f89bf5b50d7fdeed53e6d11
corn <happpy.corn.man@gmail.com>
2024-12-17 11:49:29 +0800

    blank

--- Script/Other/BlankGenerate.py
+++ Script/Other/BlankGenerate.py
@@ -4,9 +4,19 @@ import numpy as np
 FRAME_SIZE_X = 800
 FRAME_SIZE_Y = int(FRAME_SIZE_X * 0.75)
 
-RESOLUTION = 50
+RESOLUTION = 200
 
 blank = np.zeros((FRAME_SIZE_Y, FRAME_SIZE_X, 3), dtype=np.uint8)
+
+COLORS = [
+    (0, 0, 255),
+    (0, 255, 0),
+    (255, 0, 0),
+    (0, 255, 255),
+    (255, 0, 255),
+    (255, 255, 0),
+    (255, 255, 255),
+]
 
 for row in range(0, FRAME_SIZE_X, RESOLUTION):
     for col in range(0, FRAME_SIZE_X, RESOLUTION):
@@ -14,10 +24,12 @@ for row in range(0, FRAME_SIZE_X, RESOLUTION):
         y_start, y_end = row, row + RESOLUTION
         x_start, x_end = col, col + RESOLUTION
 
-        if (row+col)//RESOLUTION % 2 != 0:
-            blank[y_start:y_end, x_start:x_end] = (255, 0, 0)
-        else:
-            blank[y_start:y_end, x_start:x_end] = (0, 0, 0)
+        blank[y_start:y_end, x_start:x_end] = COLORS[(row+col)//RESOLUTION % 6]
+
+        # if (row+col)//RESOLUTION % 2 != 0:
+        #     blank[y_start:y_end, x_start:x_end] = (255, 0, 0)
+        # else:
+        #     blank[y_start:y_end, x_start:x_end] = (0, 0, 0)
 
 
-cv2.imwrite(r"Blank/2_192/blank.png", blank)
+cv2.imwrite(r"Blank/blank.png", blank)
